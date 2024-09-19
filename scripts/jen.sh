3add6c6131b455c033b2697ffb175b7c3da3c445
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2024-09-19 12:01:21 +0530

    added java installation

--- scripts/jen.sh
+++ scripts/jen.sh
@@ -1,3 +1,6 @@
+#####INSTALL JAVA 17 JRE######
+sudo apt install openjdk-17-jre-headless -y # version 17.0.12+7-1ubuntu2~24.04
+
 # Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
