469d9f1e85db4cef4efc50e4901cf3dc9020dde7
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2024-11-03 11:52:43 +0530

    Update kubectl.sh

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -1,3 +1,5 @@
+############INSTALL KUBECTL COMMAND###########
+set -x #debug mode
 sudo apt update
 sudo apt install curl -y
 curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
