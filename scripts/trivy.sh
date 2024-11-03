b77418655d72591f83386cddd9f55ad12de3b60c
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2024-11-03 11:53:09 +0530

    Update trivy.sh

--- scripts/trivy.sh
+++ scripts/trivy.sh
@@ -1,3 +1,6 @@
+############INSTALL TRIVY COMMAND###########
+set -x #debug mode
+
 sudo apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
