366159835ef172886094068500781208a882e1f4
Ubuntu <ubuntu@ip-172-31-47-234.ap-south-1.compute.internal>
2025-08-20 06:04:51 +0000

    updated the jen.sh file

--- scripts/jen.sh
+++ scripts/jen.sh
@@ -2,12 +2,23 @@
 set -x
 #sudo apt install openjdk-17-jre-headless -y # version 17.0.12+7-1 17/08/2025
 #
-sudo apt-get install openjdk-17-jre -y
+#sudo apt-get install openjdk-17-jre -y
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-21-jre
+java -version
 
 # Installing Jenkins
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+#echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+#  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#  /etc/apt/sources.list.d/jenkins.list > /dev/null
+#sudo apt-get update
+#sudo apt-get install jenkins -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
